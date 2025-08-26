c98d8f92bf651b5ba371b129b56c3c94f00dbc00
Dhara121 <2022btechaidsdhara13187@poornima.edu.in>
2025-08-26 09:55:15 +0530

    Updated server.js and added vercel config for deployment

--- server/server.js
+++ server/server.js
@@ -1,21 +1,18 @@
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
+    console.log(' MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+
+connectDB();
+
+module.exports = app;
